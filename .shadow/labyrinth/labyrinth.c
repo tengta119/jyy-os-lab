500fe09d22f128ef4ffc47ab1789818ba4e4e089
tracer-nju <tracer@nju.edu.cn>
2025-12-02 16:47:38 +0800

    Linux lbxxc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:47:38 up  2:40,  1 user,  load average: 0.11, 0.07, 0.01

--- .shadow/labyrinth/labyrinth.c
+++ .shadow/labyrinth/labyrinth.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
                 break;
             }
             palyerId = argv[i];
-            printf("当前用户id：%s \n", palyerId);
+            printf("当前用户id：%c \n", palyerId);
         }
     }
     return 0;
