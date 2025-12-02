620fe733c657113e3cb84add0aa02bee3b65b860
tracer-nju <tracer@nju.edu.cn>
2025-12-02 15:20:09 +0800

    Linux lbxxc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:20:09 up  1:16,  1 user,  load average: 0.04, 0.11, 0.11

--- .shadow/labyrinth/labyrinth.c
+++ .shadow/labyrinth/labyrinth.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[]) {
             if (i >= argc) {
                 break;
             }
+            printf(argv[i]);
             if (strcmp(argv[i], "map.txt")) {
                 printf("开始读取地图");
                 printMap();
