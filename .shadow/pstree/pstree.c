6e9001a0207addc2decb2fe0c0355f45d166d89d
tracer-nju <tracer@nju.edu.cn>
2025-12-03 17:06:50 +0800

    Linux lbxxc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:06:50 up 26 min,  1 user,  load average: 0.01, 0.04, 0.00

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <ctype.h>  // 包含 isdigit 函数
+#include <stdbool.h> // 包含 bool 类型 (C99 标准)
+
+bool isAllDigits(const char *str);
 
 int main() {
     DIR *dir = opendir("/proc");
@@ -13,13 +17,31 @@ int main() {
         return 0;
     }
 
-    printf("目录内容:\n");
+    printf("进程号:\n");
 
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL) {
         char *fileName = entry->d_name;
-        printf("\n %s", fileName);
+        if (isAllDigits(fileName)) {
+            printf("%s ", fileName);
+        }
     }
     
     closedir(dir);
+}
+
+bool isAllDigits(const char *str) {
+    if (str == NULL || *str == '\0') {
+        return false;
+    }
+
+    while (*str != '\0') {
+        if (!isdigit(*str)) {
+            return false;
+        }
+        str++;
+    }
+
+    return true;
+    
 }
