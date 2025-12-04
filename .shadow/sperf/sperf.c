f85327da6dda6701450422d06c645e5fb78227c8
tracer-nju <tracer@nju.edu.cn>
2025-12-04 15:04:30 +0800

    Linux lbxxc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:04:30 up 28 min,  1 user,  load average: 0.14, 0.08, 0.02

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -28,11 +28,11 @@ void print_top_syscalls(syscall_stats *stats, int n) {
 }
 
 int main(int argc, char *argv[]) {
-    char *exec_argv[] = { "strace", "ls", NULL, };
+    char *exec_argv[] = { "strace", "top", NULL, };
     char *exec_envp[] = { "PATH=/bin", NULL, };
     //execve("strace",          exec_argv, exec_envp);
-    //execve("/bin/strace",     exec_argv, exec_envp);
-    execve("/usr/bin/strace", exec_argv, exec_envp);
-    perror(argv[0]);
-    exit(EXIT_FAILURE);
+    execve("/bin/strace",     exec_argv, exec_envp);
+    //execve("/usr/bin/strace", exec_argv, exec_envp);
+
+    return 0;
 }
